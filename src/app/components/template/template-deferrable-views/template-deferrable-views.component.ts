4d3be9f85430ffd060799fdd71fc34815cedebe9
Edmir Lôbo <ejuniorls@outlook.com>
2024-03-18 22:16:09 -0300

    seção 08

--- src/app/components/template/template-deferrable-views/template-deferrable-views.component.ts
+++ src/app/components/template/template-deferrable-views/template-deferrable-views.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { NewComponentComponent } from '../../new-component/new-component.component';
+import { Observable, delay, of } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-template-deferrable-views',
   standalone: true,
-  imports: [NewComponentComponent],
+  imports: [NewComponentComponent, CommonModule],
   templateUrl: './template-deferrable-views.component.html',
   styleUrl: './template-deferrable-views.component.scss'
 })
 export class TemplateDeferrableViewsComponent {
   public isTrue: boolean = false;
+
+  public loadingData$: Observable<string[]> = of([
+    'item 1',
+    'item 2',
+    'item 3',
+  ]).pipe(delay(3000));
 }
