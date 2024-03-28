ddd75c06c1beb1618266a887accd4bd0a5f84517
Edmir Lôbo <ejuniorls@outlook.com>
2024-03-28 10:52:37 -0300

    seção 12

--- src/app/components/pipes/angular-pipes/angular-pipes.component.ts
+++ src/app/components/pipes/angular-pipes/angular-pipes.component.ts
@@ -3,13 +3,14 @@ import { Component, LOCALE_ID, signal } from '@angular/core';
 import { Observable, delay, of } from 'rxjs';
 
 import localePt from '@angular/common/locales/pt';
+import { CustomStringPipe } from '../../../pipes/custom-string.pipe';
 
 registerLocaleData(localePt);
 
 @Component({
   selector: 'app-angular-pipes',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, CustomStringPipe],
   templateUrl: './angular-pipes.component.html',
   styleUrl: './angular-pipes.component.scss',
   providers: [{ provide: LOCALE_ID, useValue: 'pt-BR' }]
---
+++ src/app/pipes/custom-string.pipe.ts
@@ -0,0 +1,22 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'customString',
+  standalone: true
+})
+export class CustomStringPipe implements PipeTransform {
+
+  transform(value: string, args?: string): string {
+    // console.log(value, args);
+    if (args === 'upper') {
+      return value.toUpperCase();
+    }
+
+    if (args === 'lower') {
+      return value.toLowerCase();
+    }
+
+    return value;
+  }
+
+}
